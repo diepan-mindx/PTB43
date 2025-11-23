f0bf2669ba42dd3b9d652683939256b4a20d0c7b
diepan-mindx <diepau1312@mindx.net.vn>
2025-11-23 16:40:09 +0700

    first commit

---
+++ B1/index.py
@@ -0,0 +1,12 @@
+# Nhập thông tin cơ bản -> in ra màn hình
+# đặt biến name để lưu lại tên được nhập vào
+# ten
+name = input("Ban ten gi?")  
+# tuoi
+age = input("Ban bao nhieu tuoi?") 
+# so thich
+hobbies = input("Ban co so thich nao?") 
+# in ra màn hình
+print(f"""Xin chao ban {name}, 
+      tuoi cua ban la {age} 
+      va co so thich la {hobbies}.""")
