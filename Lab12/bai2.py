5c9e73c11d0a4bbb9ac1eef0f98d2e2690f29d32
diepan-mindx <diepau1312@mindx.net.vn>
2026-03-01 15:17:51 +0700

    lab 12

---
+++ Lab12/bai2.py
@@ -0,0 +1,17 @@
+# tinh diem trung binh cho danh sach diem
+
+def tinh_diem_tb(ds_diem_str):
+    # chuyen string -> list
+    ds_diem = ds_diem_str.split(" ")
+    # chuyen het phan tu sang float (<0/ >10)
+    for i in range(len(ds_diem)):
+        ds_diem[i] = float(ds_diem[i])
+        if (not (0 <= ds_diem[i] <= 10)):
+            print("Danh sach co diem khong hop le")
+            return # ket thuc ham
+    
+    # tinh trung binh
+    result = sum(ds_diem, ds_diem[len(ds_diem) - 1])/ (len(ds_diem) + 1)
+    return round(result, 1)
+
+print(tinh_diem_tb(input()))
