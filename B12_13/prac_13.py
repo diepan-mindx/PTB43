c36fb88dbfbba89806ca90396ab3d440babff9a4
diepan-mindx <diepau1312@mindx.net.vn>
2026-03-01 15:52:17 +0700

    b13

---
+++ B12_13/prac_13.py
@@ -0,0 +1,11 @@
+def tinhluong(sogio,luonggio):
+    return sogio * luonggio
+
+def inthongin(ten, sogio, luonggio, tongluong):
+    print("Ten:", ten)
+    print("So gio lam:", sogio)
+    print("Luong 1 gio:", luonggio)
+    print("Tong luong:", tongluong)
+    
+tongluong = tinhluong(12, 150)
+inthongin("Nguyen Van A", 12, 150, tongluong)
